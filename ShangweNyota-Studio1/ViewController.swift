73d1e65bb8591e4f1e5d1252275f92117af25148
Shangwe Nyota <s.nyota@wustl.edu>
2024-08-31 11:30:15 -0500

    Initial Commit

---
+++ ShangweNyota-Studio1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShangweNyota-Studio1
+//
+//  Created by Shangwe Nyota on 8/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
