2259cb321dde9c0f67c01ff9dbafea58d125252a
Shangwe Nyota <s.nyota@wustl.edu>
2024-08-31 13:39:57 -0500

    Added random number toggle

--- ShangweNyota-Studio1/ViewController.swift
+++ ShangweNyota-Studio1/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel! //references label on story board
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func setRandomValue() {
+        label.text = String(Int.random(in:0..<10))
+    }
 
 }
 
